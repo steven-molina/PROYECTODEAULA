5c2339c950b24a8dd06feafb0c697288f6024e1b
steven-molina <101531089+steven-molina@users.noreply.github.com>
2023-03-27 23:35:28 -0500

    Add files via upload

---
+++ Instrumentos SyD/Entidades/Instrumento.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    internal class Instrumento
+    {
+
+        public String Tipo { get; set; }
+        public String Referencia { get; set; }
+        public String NroDeSerie { get; set; }
+        public Instrumento()
+        {
+
+        }
+        public Instrumento(String tipo, String referencia, String nroDeSerie)
+        {
+
+            this.Tipo = tipo;
+            this.Referencia = referencia;
+            this.NroDeSerie = nroDeSerie; 
+
+        }
+
+
+    }
+}
